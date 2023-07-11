3596b20a23377b467376c57663168262d9e12feb
WarsayT <warsay.teklebrhan@gmail.com>
2023-07-11 20:05:12 +0400

    updated, front end is done, going for server side

---
+++ client/src/components/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import Loader from './Loader'
+import Card from './Card'
+import FormField from './FormField'
+
+export {
+    Loader,
+    Card,
+    FormField
+}
